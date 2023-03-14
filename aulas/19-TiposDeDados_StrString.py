e8b0b3f6a2a86e8ab46a6be68ee1047c3c081359
JunioResende <junioresende23@gmail.com>
2023-03-14 17:40:10 -0300

    aula19

---
+++ aulas/19-TiposDeDados_StrString.py
@@ -0,0 +1,12 @@
+print('Aspas Simples')
+print("==================================")
+print("Aspas Duplas")
+print("==================================")
+print("Escape")
+print("Junio \"Resende\"")
+print("==================================")
+print("r")#Utilizado quando voce quer que todos os caracteres aparecam
+print(r"Junio \"Resende\"")
+print("==================================")
+print("dentro de aspas podemos utilizar outra aspa")#Basta inverter a aspa de delimitacao usada
+print("Junio 'Resende'")
