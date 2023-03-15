949eb34dd441c1cf1a89acb0a0727d6991fcc211
JunioResende <junioresende23@gmail.com>
2023-03-15 12:06:21 -0300

    aula34

---
+++ aulas/secao3/34-IntroducaoIfElseElif.py
@@ -0,0 +1,13 @@
+# if / elif / else
+# se / se nao se / se nao
+
+entrada = input("Voce vai entrar ou sair do sistema? ")
+
+if entrada == "entrar": #Verifica se uma condicao e valida(verdadeira)
+    print("Voce entrou no sistema!") #Se for verdade ira executar esse trecho
+elif entrada == "sair": #Verifica se a outra condicao e valida
+    print("Voce saiu do sistema!")#Se for verdade ira executar esse trecho
+else:
+    print("Voce nao digitou se quer entrar ou sair do sistema!")#Se nenhuma condicao for valida executara esse trecho
+
+
