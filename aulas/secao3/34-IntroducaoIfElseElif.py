04774c7d30565314dd40c8a6111b6c675b2d4ce3
JunioResende <junioresende23@gmail.com>
2023-03-15 13:55:43 -0300

    exercicio

--- aulas/secao3/34-IntroducaoIfElseElif.py
+++ aulas/secao3/34-IntroducaoIfElseElif.py
@@ -3,11 +3,10 @@
 
 entrada = input("Voce vai entrar ou sair do sistema? ")
 
-if entrada == "entrar": #Verifica se uma condicao e valida(verdadeira)
-    print("Voce entrou no sistema!") #Se for verdade ira executar esse trecho
-elif entrada == "sair": #Verifica se a outra condicao e valida
-    print("Voce saiu do sistema!")#Se for verdade ira executar esse trecho
+if entrada == "entrar":  # Verifica se uma condicao e valida(verdadeira)
+    print("Voce entrou no sistema!")  # Se for verdade ira executar esse trecho
+elif entrada == "sair":  # Verifica se a outra condicao e valida
+    print("Voce saiu do sistema!")  # Se for verdade ira executar esse trecho
 else:
-    print("Voce nao digitou se quer entrar ou sair do sistema!")#Se nenhuma condicao for valida executara esse trecho
-
-
+    # Se nenhuma condicao for valida executara esse trecho
+    print("Voce nao digitou se quer entrar ou sair do sistema!")
---
+++ aulas/secao3/38-exercicioComparacao.py
@@ -0,0 +1,9 @@
+primeiro_valor = input('Digite o primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor > segundo_valor:
+    print("O primeiro valor e maior que o segundo valor")
+elif primeiro_valor < segundo_valor:
+    print("O segundo valor e maior que o primeiro valor")
+else:
+    print("Os valores sao iguais")
