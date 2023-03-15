a5a546b910c89b29b11e99303a77fe7f9b51b4d9
JunioResende <junioresende23@gmail.com>
2023-03-15 09:44:15 -0300

    exercicio22

---
+++ aulas/secao3/22-ExercicioVariaveisETipoDeDados.py
@@ -0,0 +1,14 @@
+nome = "Junio"
+sobrenome = "Resende"
+idade = 2022 - 1990
+ano_nascimento = 1990
+maior_de_idade = idade >= 18
+altura_metros = 1.88
+
+
+print("Nome: ", nome, type(nome))
+print("Sobrenome: ", sobrenome, type(sobrenome))
+print("Idade: ", idade, type(idade))
+print("Ano de nascimento: ", ano_nascimento, type(ano_nascimento))
+print("E maior de idade? ", maior_de_idade, type(maior_de_idade))
+print("Altura: ", altura_metros, type(altura_metros))
